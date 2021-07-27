4de44f292df0acc047ba306ea6c3a5bbf245754d
msjun23 <msjun23@gmail.com>
2021-07-27 17:17:47 +0900

    Publish LaserScan data

--- scripts/webrtc_ros.py
+++ scripts/webrtc_ros.py
@@ -24,42 +24,42 @@ from sensor_msgs.msg import LaserScan
 
 
 def ScanData(data):
-    rospy.loginfo(rospy.get_caller_id() + 'Lidar LaserScan data:%s', data)
+    #rospy.loginfo(rospy.get_caller_id() + 'Lidar LaserScan data:%s', data.ranges[:3])
 
-    ##로봇 파라미터
+    # Sensing Data & Robot parameters
     current_location = [1,1,0]
-    LIDAR_data = data
+    LIDAR_data = data.ranges[:3]
     remote_message = []
 
     #while True:
     send_json = json.dumps({'LIDAR_data': LIDAR_data})
-    driver.execute_script('sendData('+send_json+')')#보내기
+    driver.execute_script('sendData('+send_json+')')                        # Send Data
     remote_message = driver.execute_script("return getData()")
-    print(send_json, '\t', remote_message)
+    print(send_json)
 
 if __name__=="__main__":
     # Setup webdriver & network
-    options = webdriver.ChromeOptions()             # 옵션을 추가합니다.
-    # options.add_argument("--disable-infobars")    #infobar를 안씁니다.
-    options.add_argument('--start-fullscreen')      # F11풀화면으로 시작합니다.
-    options.add_argument("--disable-extensions")    # 크롬 확장 프로그램을 안씁니다.
+    options = webdriver.ChromeOptions()                                     # Webdriver option
+    # options.add_argument("--disable-infobars")                            # Don't use infobar
+    options.add_argument('--start-fullscreen')                              # Start at F11; Full screan
+    options.add_argument("--disable-extensions")                            # Disable Chrome extension program
     options.add_experimental_option("prefs", { \
-        "profile.default_content_setting_values.media_stream_mic": 1,       # 미디어 마이크 허용
-        "profile.default_content_setting_values.media_stream_camera": 1,    # 미디어 카메라 허용
-        "profile.default_content_setting_values.geolocation": 1,            # 현재위치 파악허용
-        "profile.default_content_setting_values.notifications": 1           # 알림허용
+        "profile.default_content_setting_values.media_stream_mic": 1,       # Able Media Mike
+        "profile.default_content_setting_values.media_stream_camera": 1,    # Able Media Camera
+        "profile.default_content_setting_values.geolocation": 1,            # Able Location
+        "profile.default_content_setting_values.notifications": 1           # Able Alert
     })
 
     current_os = platform.system()
 
-    if current_os == "Windows":  # 현재 os가 윈도우라면, 윈도우용 드라이버를 쓴다.
+    if current_os == "Windows":  # Driver for window
         driver = webdriver.Chrome(executable_path='./Windows/chromedriver', options=options)
-    elif current_os == "Linux":  # 현재 os가 리눅스라면, 리눅스용
+    elif current_os == "Linux":  # Driver for Linux
         driver = webdriver.Chrome(executable_path='./Linux/chromedriver', options=options)
 
     abs_path = os.path.abspath("./")
 
-    driver.get(url='file:///'+abs_path+'/rail.html')  # 사이트에 접속합니다.
+    driver.get(url='file:///'+abs_path+'/rail.html')  # Connect to web site
 
 
     # Setup ROS node
